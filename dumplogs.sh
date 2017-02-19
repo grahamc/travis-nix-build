ab25a129a46336c5dc8254593ddc3ba1e3bf7abf
Graham Christensen <graham@grahamc.com>
2017-02-19 11:28:20 -0500

    :)

---
+++ dumplogs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p tree --pure
+
+drvs() {
+    find /nix/var/log/nix/drvs/ -type f -name '*.drv.bz2' -print0
+}
+
+outputs() {
+    nix-store -q --outputs "$1"
+}
+
+drvs | (while read -d "" drv; do
+              drvname=$(basename -s ".bz2" "$drv")
+              drvpath="/nix/store/$drvname"
+              if ! test -f "$drvpath"; then
+                  continue
+              fi
+
+              worked=0
+              echo "$drvpath =>"
+              for out in $(outputs "$drvpath"); do
+                  echo " -> $out"
+                  if test -e "$out"; then
+                      worked=1
+                  fi
+              done
+              echo "log:"
+              nix-store --read-log "$drvpath" | sed -e "s/^/ --> /"
+              if [ "$worked" -eq 0 ]; then
+                  echo "Build failed :("
+              else
+                  echo "Build passed!"
+              fi
+          done)
